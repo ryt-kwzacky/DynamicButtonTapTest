e93b9aa1a3599eb4199681ebb0f5f14c0611169f
RyutoKawasaki <various.use.0416@gmail.com>
2019-11-21 19:16:33 +0900

    first commit

---
+++ DynamicButtonTapTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DynamicButtonTapTest
+//
+//  Created by Ryuton on 2019/11/21.
+//  Copyright © 2019 Ryuton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
